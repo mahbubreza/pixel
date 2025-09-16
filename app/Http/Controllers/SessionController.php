25e4119769c793ae31fa928cd942eaee4d629776
mahbubreza <mahbubrezachoudhury@gmail.com>
2025-09-16 16:25:17 +0600

    from office

---
+++ app/Http/Controllers/SessionController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\ValidationException;
+
+class SessionController extends Controller
+{
+    
+    public function create()
+    {
+        return view('auth.login');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $attributes = request()->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required']
+        ]);
+        if(! Auth::attempt($attributes)){
+            throw ValidationException::withMessages([
+                'email' => 'Sorry, The credentials do not match.'
+            ]);
+        }
+        request()->session()->regenerate();
+        return redirect('/');
+    }
+
+    public function destroy(string $id)
+    {
+        Auth::logout();
+        return redirect('/');
+    }
+}
